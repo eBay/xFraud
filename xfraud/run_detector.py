36e1cbf14c02549c1100c1c042ca4012c9b8e184
zitao zhang <zitzhang@ebay.com>
2021-06-01 14:21:30 +0800

    add licenses info before all python files

--- xfraud/run_detector.py
+++ xfraud/run_detector.py
@@ -1,11 +1,20 @@
+# Copyright 2020-2021 eBay Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     https://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 import os
-import gzip
 import logging
-import shutil
 import tempfile
 import subprocess
 from functools import partial
-from collections import defaultdict
 import glob
 
 logging.basicConfig(format='%(asctime)s | %(levelname)s | %(name)s | %(message)s')
