3877db6e8f8256c2dee38947b9acdaf34ce419ec
zitzhang <zitzhang@ebay.com>
2021-09-01 08:20:45 +0000

    update requirements files

--- scripts/install-env-publish.sh
+++ scripts/install-env-publish.sh
@@ -8,7 +8,7 @@ conda install pytorch==1.8.1 torchvision==0.9.1 torchaudio==0.8.1 cudatoolkit=10
 conda install ignite -c pytorch -y
 
 pip install --no-index \
-    torch-scatter torch-sparse torch-cluster torch-spline-conv -f \
-    https://pytorch-geometric.com/whl/torch-1.8.1+cu101.html
+    torch-scatter torch-sparse torch-cluster torch-spline-conv \
+    -f https://pytorch-geometric.com/whl/torch-1.8.1+cu101.html
 
 pip install torch-geometric==1.7.2
